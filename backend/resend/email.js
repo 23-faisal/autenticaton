c62ebdc5cea4474651e4a36056603e0ae1977441
23-faisal <a.faisal23r@gmail.com>
2024-12-04 20:58:54 +0600

    send verification email using react email

--- backend/resend/email.js
+++ backend/resend/email.js
@@ -1,3 +1,4 @@
+import { verificationTokenEmailTemplate } from "./email-templates.js";
 import { resend } from "./resend.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -6,7 +7,10 @@ export const sendVerificationEmail = async (email, verificationToken) => {
       from: "Acme <onboarding@resend.dev>",
       to: [email],
       subject: "Verify your email address",
-      html: `Verify your email address with this token: ${verificationToken}`,
+      html: verificationTokenEmailTemplate.replace(
+        "{verificationToken}",
+        verificationToken
+      ),
     });
     console.log("Email sent response:", response);
   } catch (error) {
