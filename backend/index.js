84dc3db80bc2bc052f8eec364999071ad7d7f597
23-faisal <a.faisal23r@gmail.com>
2024-12-04 10:03:38 +0600

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import { app } from "./app.js";
+import { connectToDatabase } from "./config/database.js";
 import "dotenv/config";
 
 const port = process.env.PORT || 5000;
+
+connectToDatabase();
 
 app.listen(port, () => {
   console.log(`App is running at port ${port}`);
